2ef62a5a51126fef7db02737de5bfc70ffdef0cb
Tanushree-coder <tanushree950444@gmail.com>
2023-08-10 17:10:18 +0530

    final deploy

--- controller/Auth.js
+++ controller/Auth.js
@@ -3,9 +3,8 @@ const crypto = require('crypto');
 const { sanitizeUser } = require('../services/common');
 const jwt = require('jsonwebtoken');
 
-exports.createUser = async (req, res) => 
-{
-    try {
+exports.createUser = async (req, res) => {
+  try {
     const salt = crypto.randomBytes(16);
     crypto.pbkdf2(
       req.body.password,
@@ -17,20 +16,19 @@ exports.createUser = async (req, res) =>
         const user = new User({ ...req.body, password: hashedPassword, salt });
         const doc = await user.save();
 
-        req.login(sanitizeUser(doc), (err) => {  
-            // this also calls serializer and adds to session
+        req.login(sanitizeUser(doc), (err) => {
+          // this also calls serializer and adds to session
           if (err) {
             res.status(400).json(err);
           } else {
             const token = jwt.sign(sanitizeUser(doc), process.env.JWT_SECRET_KEY);
-
-            res.cookie('jwt', token, {
+            res
+              .cookie('jwt', token, {
                 expires: new Date(Date.now() + 3600000),
                 httpOnly: true,
               })
               .status(201)
               .json({id:doc.id, role:doc.role});
-              // .json(token)
           }
         });
       }
@@ -40,23 +38,21 @@ exports.createUser = async (req, res) =>
   }
 };
 
-exports.loginUser = async (req, res) => 
-{
-    const user=req.user
-     res.cookie('jwt', user.token, {
+exports.loginUser = async (req, res) => {
+  const user = req.user
+  res
+    .cookie('jwt', user.token, {
       expires: new Date(Date.now() + 3600000),
       httpOnly: true,
     })
     .status(201)
-    .json({id:user.id,role:user.role});
-    // .json(user.token)
+    .json({id:user.id, role:user.role});
 };
 
 exports.checkAuth = async (req, res) => {
-  // console.log(req.user)
-    if(req.user){
-      res.json(req.user);
-    } else{
-      res.sendStatus(401);
-    }
+  if(req.user){
+    res.json(req.user);
+  } else{
+    res.sendStatus(401);
+  }
 };
